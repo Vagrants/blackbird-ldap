36519659338cba9c7e75c9763182daef90b713c5
Ryo Tagami <tagami_ryo@cyberagent.co.jp>
2014-01-07 12:26:38 +0900

    Change method name according to upstream.
    Upstream has changed method name as follows:
    * looped_method -> build_items
    * gethostname -> detect_hostname
    so, we are following this.
    
    Also, setup.py is now actually working.

--- setup.py
+++ setup.py
@@ -8,15 +8,20 @@ except ImportError:
 
 setup(
     name='blackbird-ldap',
-    version='0.1.0',
-    description=(
-        'Get monitorring stats  of ldap for blackbird '
-        'by using ldapsearch.'
-    ),
+    version='0.1.1',
+    description='Blackbird plugin for monitoring slapd runtime information.',
     author='Vagrants',
     author_email='vagrants.git@gmail.com',
     url='https://github.com/Vagrants/blackbird-ldap',
     data_files=[
         ('/opt/blackbird/plugins', ['ldap.py'])
-    ]
+    ],
+    install_requires=[
+        'blackbird>=0.3.2',
+        'python-ldap',
+    ],
+    dependency_links=[
+        'https://github.com/Vagrants/blackbird'
+        '/tarball/master#egg=blackbird-0.3.2',
+    ],
 )
